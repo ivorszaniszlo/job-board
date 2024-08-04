da71cdbe943635f3b4d3e93a06f27496d1f0e1b4
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-08-04 16:08:43 +0200

    feat: add filtering jobs - form & searching for text in job posts

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -2,4 +2,7 @@
     <x-breadcrumbs class="mb-4"
     :links="['Jobs' => route('jobs.index'), $job->title => '#']" />
     <x-job-card :$job />
+    <p class="text-sm text-slate-500">
+        {!! nl2br(e($job->description)) !!}
+    </p>
 </x-layout>
