dced0d865d5783b914a53c564d375e0f2e11e7c4
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-08-15 12:13:53 +0200

    feat: add authentication controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,9 @@ Route::get('', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+
+Route::resource('auth', AuthController::class)
+    ->only(['create', 'store']);            
 
