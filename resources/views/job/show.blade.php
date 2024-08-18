1ef6ee4dd66ed7cf289e4238c9028a1f1eb88204
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-08-18 17:51:22 +0200

    feat: add applying for jobs - job policy

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -2,9 +2,13 @@
     <x-breadcrumbs class="mb-4"
         :links="['Jobs' => route('jobs.index'), $job->title => '#']" />
     <x-job-card :$job >
-    <p class="mb-4 text-sm text-slate-500">
-        {!! nl2br(e($job->description)) !!}
-    </p>
+        <p class="mb-4 text-sm text-slate-500">
+            {!! nl2br(e($job->description)) !!}
+        </p>
+
+        <x-link-button :href="route('job.application.create', $job)">
+            Apply
+        </x-link-button>
     </x-job-card>
 
     <x-card class="mb-4">
