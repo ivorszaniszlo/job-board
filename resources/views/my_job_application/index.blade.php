b3a2ebbd31b39e7ac392a0606e20a5efe47b183f
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-08-19 19:55:03 +0200

    feat: add my applications applicant count, average asking salary

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -4,6 +4,23 @@
 
     @forelse ($applications as $application)
         <x-job-card :job="$application->job">
+            <div class="flex itms-center justify-between text-sm text-slate-500">
+                <div>
+                    <div>
+                        Applied {{ $application->created_at->diffForHumans() }}
+                    </div>
+                    <div>
+                        Other {{Str::plural('application', $application->job->job_applications_count - 1) }}
+                        {{ $application->job->job_applications_count - 1 }}
+                    </div>
+                    <div>
+                    Your asking salary: ${{ number_format($application->expected_salary) }}
+                    </div>
+                    <div>
+                        Average asking salary: ${{ number_format($application->job->job_applications_avg_expected_salary) }}
+                    </div>
+                </div>
+            </div>
             
         </x-job-card>
     @empty
