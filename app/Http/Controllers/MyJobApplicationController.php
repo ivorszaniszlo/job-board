474422f366bd3ff23b6f54fe9ec4aa611333f1f2
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-08-20 18:03:38 +0200

    feat: add my applications cancelling

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class MyJobApplicationController extends Controller
@@ -28,8 +30,12 @@ class MyJobApplicationController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success', 'Job application removed.'
+        );
     }
 }
--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -20,10 +20,23 @@
                         Average asking salary: ${{ number_format($application->job->job_applications_avg_expected_salary) }}
                     </div>
                 </div>
+                <div>
+                    <form action="{{ route('my-job-applications.destroy', $application) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Cancel</x-button>
+                    </form>
+                </div>
             </div>
-            
         </x-job-card>
     @empty
-
+        <div class="rounded-md border border-dashed border-slate-300 p-8"
+            <div class="text-center text-sm font-medium">
+                No job application yet
+            </div>
+            <div class="text-center">
+                Go find some jobs <a href="{{ route('jobs.index') }}" class="text-indigo-500 hover:underline">here</a>
+            </div>
+        </div>
     @endforelse
 </x-layout>
